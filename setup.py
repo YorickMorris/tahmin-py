1d96b37105f8d53f539b9042a5868b318d639be0
YorickMorris <54893119+YorickMorris@users.noreply.github.com>
2022-02-22 14:19:54 +0300

    changes on setup.py
    changes on setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 import os
-from setuptools import setup
 
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
@@ -10,12 +9,6 @@ setup(
     author = "Fatih Emre KAYA",
     author_email = "kaa_femre_1998@hotmail.com",
     description = ("Tahmin Uygulaması"),
-    license = "BSD",
     keywords = "example test tutorial",
     url = "https://git.heroku.com/test-tahmin.git",
-    classifiers=[
-        "Development Status :: 1 - Alpha",
-        "Topic :: Utilities",
-        "License :: OSI Approved :: BSD License",
-    ],
 )
