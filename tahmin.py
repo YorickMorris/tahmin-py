ec1fe2a2983cf0998199903800bdf0a7085c9f6e
YorickMorris <54893119+YorickMorris@users.noreply.github.com>
2022-02-22 13:18:24 +0300

    init
    tahmin.py

---
+++ tahmin.py
@@ -0,0 +1,24 @@
+import random
+import sys
+import os
+
+workingdirectory=os.getcwd()
+#Python method getcwd() returns current working directory of a process.
+#sys.argv() is an array for command line arguments in Python.
+print('Link: ',sys.argv[1])
+sayilar = range(0, 100)
+sayi = random.choice(sayilar)
+print(sayi)
+secim = None
+
+
+for i in range(10):
+    print(str(i + 1) + ". denemeniz : ")
+    #secim = input()
+    secim=sys.argv[1]
+    if(str(secim) == str(sayi)):
+        print("Tebrikler dogru tahmin !")
+        break
+
+    else:
+        print("Bilemediniz")
